c127253b9de6564a50efc66c16df31756218ac23
panatest1 <amarinpratumwan@gmail.com>
2016-10-14 15:03:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<form action="saveformattest.php" method="post">
+ Name: <input type="text" name="youname" id="youname" /><br />
+ E-mail: <input type="text" name="youemail" id="youemail" /><br />
+ Latest studies: <select name="studies" id="studies">
+  <option value="">---</option>
+  <option value="elementary school">elementary school</option>
+  <option value="secondary school">secondary school</option>
+  <option value="college">college</option>
+  <option value="academic school">academic school</option>
+ </select><br />
+ Marital status: <input type="radio" name="civilstate" id="civilstate" value="single" />Single
+   <input type="radio" name="civilstate" id="civilstate" value="married" />Married<br />
+   <input type="submit" id="submit" value="Send" />
+</form>
